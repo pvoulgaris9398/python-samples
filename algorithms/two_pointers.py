f3084cf62e50df150de0283b0cb08951d6882a40
Pete Voulgaris <petevoulgaris@gmail.com>
2025-11-16 16:41:56 -0500

    added _generalized_ version of the algorithm

--- algorithms/two_pointers.py
+++ algorithms/two_pointers.py
@@ -14,32 +14,102 @@ def is_palindrome(s: str) -> bool:
     return True
 
 
+"""
+Using generalized version of "two-pointer" algorith
+"""
+
+
+def is_palindrome_2(s: str) -> bool:
+    if s is None:
+        return False
+
+    def next_left_index(s: str, left):
+        return left + 1
+
+    def next_right_index(s: str, right):
+        return right - 1
+
+    def check_condition(left_value, right_value):
+        return left_value == right_value
+
+    return two_pointer_template2(s, next_left_index, next_right_index, check_condition)
+
+
+"""
+Generalized version the algorithm
+"""
+
+
+def two_pointer_template2(
+    input: list, next_left_index, next_right_index, check_condition
+):
+    left = 0
+    right = len(input) - 1
+    while left < right:
+        left = next_left_index(input, left)
+        right = next_right_index(input, right)
+        if not check_condition(input[left], input[right]):
+            return False
+
+    return True
+
+
+def two_pointer_template1(input: list, is_valid, check_condition):
+    left = 0
+    right = len(input) - 1
+    while left < right:
+        while not is_valid(input[left]):
+            left = left + 1
+        while not is_valid(input[right]):
+            right = right - 1
+        if not check_condition(input[left], input[right]):
+            return False
+        left = left + 1
+        right = right - 1
+
+    return True
+
+
 def test0():
     input = None
     result = is_palindrome(input)
 
-    print(f"{input} {result}")
+    print(f"(is_palindrome): {input} {result}")
 
 
 def test1():
     input = "abcdcba"
     result = is_palindrome(input)
 
-    print(f"{input} {result}")
+    print(f"(is_palindrome): {input} {result}")
 
 
 def test2():
     input = "amanaplanacanalpanama"
     result = is_palindrome(input)
 
-    print(f"{input} {result}")
+    print(f"(is_palindrome): {input} {result}")
 
 
 def test3():
     input = "kali"
     result = is_palindrome(input)
 
-    print(f"{input} {result}")
+    print(f"(is_palindrome): {input} {result}")
+
+
+def test4():
+    input = "kali"
+    result = is_palindrome_2(input)
+
+    print(f"(is_palindrome_2): {input} {result}")
+
+
+def test5():
+    input = "abcdcba"
+    result = is_palindrome_2(input)
+
+    print(f"(is_palindrome_2): {input} {result}")
 
 
 def main():
@@ -48,6 +118,8 @@ def main():
     test1()
     test2()
     test3()
+    test4()
+    test5()
 
 
 if __name__ == "__main__":
